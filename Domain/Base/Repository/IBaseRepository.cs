9e15732f8717e8cecc7c6974b96a283c1b0f527e
Eduardo1219 <53283218+Eduardo1219@users.noreply.github.com>
2024-05-11 18:32:10 -0300

    feat: first commit

---
+++ Domain/Base/Repository/IBaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Base.Entity;
+using System.Linq.Expressions;
+
+namespace Domain.Base.Repository
+{
+    public interface IBaseRepository<TB> where TB : BaseEntity
+    {
+        Task AddAsync(TB entity);
+
+        Task RemoveAsync(TB entity);
+
+        Task RemoveByIdAsync(Guid id);
+
+        Task UpdateAsync(TB entity);
+
+        Task<TB> GetByIdAsync(Guid id);
+
+        Task<IList<TB>> GetPagedAsync(Expression<Func<TB, bool>> search, int take, int skip, Expression<Func<TB, dynamic>> orderDesc);
+
+        Task<IList<TB>> GetAsync(Expression<Func<TB, bool>> search, Expression<Func<TB, dynamic>> orderDesc);
+
+        Task<IList<TB>> GetAsync(Expression<Func<TB, bool>> search);
+
+        Task<TB> GetFirstAsync(Expression<Func<TB, bool>> search);
+    }
+}
